1cb90c72154dc130b8357d48c31644b909946aba
Deivid Steven Gonzalez <deividclar@gmail.com>
2023-07-30 21:11:43 -0500

    Add screen CryptoScreen with mock data

---
+++ src/screens/CryptoListScreen/index.tsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import {Crypto} from '../../components/atoms/CryptoItem';
+import CryptoListTemplate from '../../components/templates/CryptoListTemplate';
+
+const CryptoListScreen = () => {
+  const [cryptoData, setCryptoData] = useState<Crypto[]>([]);
+
+  useEffect(() => {
+    const dummyCryptoList: Crypto[] = [
+      {
+        id: '1',
+        icon: 'bitcoin',
+        abbreviation: 'BTC',
+        name: 'Bitcoin',
+        price: 42000,
+        lastHourChange: 0.5,
+      },
+      {
+        id: '2',
+        icon: 'ethereum',
+        abbreviation: 'ETH',
+        name: 'Ethereum',
+        price: 2800,
+        lastHourChange: -0.2,
+      },
+    ];
+    setCryptoData(dummyCryptoList);
+  }, []);
+  return <CryptoListTemplate data={cryptoData} />;
+};
+
+export default CryptoListScreen;
