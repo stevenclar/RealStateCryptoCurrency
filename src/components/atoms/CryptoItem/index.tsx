722f534cb508d8c8353ac853cc0b8e4d279d5076
Deivid Steven Gonzalez <deividclar@gmail.com>
2023-07-30 22:44:23 -0500

    Add crypto image

--- src/components/atoms/CryptoItem/index.tsx
+++ src/components/atoms/CryptoItem/index.tsx
@@ -1,8 +1,10 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useState} from 'react';
 import {View} from 'react-native';
-import {Avatar, Title, Paragraph, useTheme} from 'react-native-paper';
+import {Title, Paragraph, useTheme, Avatar} from 'react-native-paper';
+import FastImage from 'react-native-fast-image';
 import styles from './styles';
 import {currencyFormat} from '../../../utils/currency/currencyFormat';
+import {getCurrencyImageUrl} from '../../../utils/crypto/getUrlImage';
 
 export interface Crypto {
   id: string;
@@ -16,12 +18,12 @@ export interface Crypto {
 interface CryptoItemProps extends Crypto {}
 
 const CryptoItem = ({
-  icon,
   abbreviation,
   name,
   price,
   lastHourChange,
 }: CryptoItemProps) => {
+  const [errorImage, setErrorImage] = useState(false);
   const formatPrice = useMemo(() => currencyFormat(price), [price]);
   const isPositive = useMemo(() => lastHourChange >= 0, [lastHourChange]);
   const lastHourChangeSign = useMemo(
@@ -32,6 +34,16 @@ const CryptoItem = ({
     () => (isPositive ? 'green' : 'red'),
     [isPositive],
   );
+
+  const currencyUrlImage = useMemo(
+    () => getCurrencyImageUrl(abbreviation),
+    [abbreviation],
+  );
+
+  React.useEffect(() => {
+    console.log(currencyUrlImage);
+  }, [currencyUrlImage]);
+
   const {
     colors: {backdrop},
   } = useTheme();
@@ -39,7 +51,17 @@ const CryptoItem = ({
     <View style={styles.container}>
       <View style={styles.content}>
         <View style={styles.leftContent}>
-          <Avatar.Icon size={40} icon={icon} />
+          {currencyUrlImage && !errorImage && (
+            <FastImage
+              source={{uri: currencyUrlImage}}
+              resizeMode={FastImage.resizeMode.contain}
+              onError={() => {
+                setErrorImage(true);
+              }}
+              style={styles.image}
+            />
+          )}
+          {errorImage && <Avatar.Icon size={40} icon="bitcoin" />}
           <View style={styles.titleContainer}>
             <Title>{abbreviation}</Title>
             <Paragraph style={{color: backdrop}}>{name}</Paragraph>
