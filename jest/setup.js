efefee4a27a451dff0918386461a692597f1bbcf
Deivid Steven Gonzalez <deividclar@gmail.com>
2023-08-01 23:08:45 -0500

    Add @testing-library/jest-native

--- jest/setup.js
+++ jest/setup.js
@@ -1,7 +1,1 @@
-/* eslint-disable no-undef */
-jest.mock('react-native-bootsplash', () => {
-  return {
-    hide: jest.fn().mockResolvedValueOnce(),
-    getVisibilityStatus: jest.fn().mockResolvedValue('hidden'),
-  };
-});
+import '@testing-library/jest-native/extend-expect';
